4c2e276f1e74ffa6e2be5c12177534645cfbdb2a
Arman Dindar Safa <armandindar6@gmail.com>
2025-03-10 03:35:47 +0330

    Add files via upload

---
+++ scalarTransportCavity/runeif.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd "${0%/*}" || exit                                # Run from this directory
+. ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions        # Tutorial run functions
+#------------------------------------------------------------------------------
+
+
+# Change to the case directory
+cd "${0%/*}" || exit 1
+
+# Run blockMesh
+echo "Running blockMesh..."
+blockMesh > log.blockMesh 2>&1
+
+# Check if blockMesh ran successfully
+if [ $? -ne 0 ]; then
+    echo "blockMesh failed. Check log.blockMesh for details."
+    exit 1
+fi
+
+# Run energyIcoFoam solver
+echo "Running energyIcoFoam..."
+energyIcoFoam > log.energyIcoFoam 2>&1
+
+# Check if energyIcoFoam ran successfully
+if [ $? -ne 0 ]; then
+    echo "energyIcoFoam failed. Check log.energyIcoFoam for details."
+    exit 1
+fi
+
+# Create Paraview executable
+echo "Creating Paraview executable..."
+touch results.foam
